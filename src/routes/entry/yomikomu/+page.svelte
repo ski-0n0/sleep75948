f120523ab0fed26cde2fce875f94723528c682bc
tsunaki.okubo <tsunaki.okubo@uzabase.com>
2023-06-06 17:17:37 +0900

    ば〜〜せる

--- src/routes/entry/yomikomu/+page.svelte
+++ src/routes/entry/yomikomu/+page.svelte
@@ -5,11 +5,11 @@
 <svelte:head>
 	<title>よみこむ</title>
 	<meta name="description" content="" />
-	<meta property="og:title" content="よみこむ" />
+	<meta property="og:title" content="よみこむ &#9822;" />
 	<meta property="og:type" content="article" />
 	<meta property="og:url" content="https://sleep75948-jhg8i7mzz-ski-0n0.vercel.app/entry/yomikomu" />
 	<meta property="og:image" content="" />
-	<meta property="og:site_name" content="よみこむ" />
+	<meta property="og:site_name" content="よみこむ &#9822;" />
 	<meta property="og:description" content="" />
 	<meta property="og:locale" content="ja_JP" />
 </svelte:head>
